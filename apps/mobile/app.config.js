b79982165be0be90b918cfd2174b38e4c2f51b30
Paul <paulpep2002@gmail.com>
2025-04-08 15:29:37 +0300

    building eas

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,6 +1,12 @@
+/* eslint-env node */
+/* global process */
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default ({ config }) => ({
   ...config,
-  name: "funzasasa",
+  name: "myTutorApp",
   slug: "mobile",
   platforms: ["ios", "android"],
   entryPoint: "./index.tsx",
@@ -12,7 +18,7 @@ export default ({ config }) => ({
   },
   extra: {
     ...config.extra,
-    backendUrl: process.env.EXPO_PUBLIC_BACKEND_URL || "http://localhost:4000",
+    backendUrl: process.env.EXPO_PUBLIC_BACKEND_URL || "https://localhost:4000",
     eas: {
       projectId: "f9e88ea7-6ab8-4385-84a7-dc06feb64bca"
     }
