aef126d3c29d4747f1a0b8f7d5009a23e4b0dcdc
Paul <paulpep2002@gmail.com>
2025-04-29 14:43:14 +0300

    build new

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,21 +2,21 @@
 
 const { getDefaultConfig } = require('@expo/metro-config');
 
-// Get the default Expo config
-const config = getDefaultConfig(__dirname);
-
-// Add your custom watchFolders if needed (optional!)
 const path = require('path');
 const workspaceRoot = path.resolve(__dirname, '../../');
+const projectRoot = __dirname;
 
-config.watchFolders = [
-  path.resolve(workspaceRoot, 'packages/shared'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
 
 config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
+  path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
+
+if (!config.resolver.assetExts.includes('cjs')) {
+  config.resolver.assetExts.push('cjs');
+}
 
 module.exports = config;
