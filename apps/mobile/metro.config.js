66b02ed9f14c1a033f9d696e40886948d288957a
Paul <paulpep2002@gmail.com>
2025-06-28 20:26:18 +0300

    metro restore

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,31 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// apps/mobile/metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const projectRoot   = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
-module.exports = config;
+module.exports = (async () => {
+  const config = await getDefaultConfig(projectRoot);
+
+  // Watch the monorepo root for shared code
+  config.watchFolders = [workspaceRoot];
+
+  // Resolve modules from both mobile/node_modules and root/node_modules
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+  ];
+  config.resolver.disableHierarchicalLookup = true;
+
+  // Map all imports to the root node_modules first
+  config.resolver.extraNodeModules = new Proxy(
+    {},
+    {
+      get: (_, name) =>
+        path.resolve(workspaceRoot, 'node_modules', name),
+    }
+  );
+
+  return config;
+})();
