d7929835cdbb3f38ccde0ae2f282425aca92e7a1
Paul <paulpep2002@gmail.com>
2026-03-29 22:11:40 +0300

    updating next backend url

--- apps/web/.open-next/cloudflare/init.js
+++ apps/web/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1774774665126,
+    __BUILD_TIMESTAMP_MS__: 1774810938517,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
